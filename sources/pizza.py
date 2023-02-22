d8e256a31c5cea6f4b26ad86c30f73e8114b56d0
Paul-prog44 <saintouenfr@hotmail.com>
2023-02-22 12:06:04 +0100

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,10 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'hawaienne' : {
+        'ingredients': {'parmesan': 3, 'basil': 4, 'oregano': 3},
+        'price': 15,
     }
+
 }
